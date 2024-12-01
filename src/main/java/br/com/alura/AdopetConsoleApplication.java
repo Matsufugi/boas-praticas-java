5d108f258ae714f2e8e95003b4e2a9229d15bceb
Matsufugi <matt.my.23@gmail.com>
2024-12-01 19:16:17 -0300

    Refatoração dos métodos ListasAbrigos e CadastrarAbrigo para a pasta services

--- src/main/java/br/com/alura/AdopetConsoleApplication.java
+++ src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura;
 
+import br.com.alura.services.AbrigoService;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -17,6 +18,10 @@ import java.util.Scanner;
 public class AdopetConsoleApplication {
 
     public static void main(String[] args) {
+
+        AbrigoService service = new AbrigoService();
+
+
         System.out.println("##### BOAS VINDAS AO SISTEMA ADOPET CONSOLE #####");
         try {
             int opcaoEscolhida = 0;
@@ -32,9 +37,9 @@ public class AdopetConsoleApplication {
                 opcaoEscolhida = Integer.parseInt(textoDigitado);
 
                 if (opcaoEscolhida == 1) {
-                    ListarAbrigos();
+                    service.ListarAbrigos();
                 } else if (opcaoEscolhida == 2) {
-                    CadastrarAbrigo();
+                    service.CadastrarAbrigo();
                 } else if (opcaoEscolhida == 3) {
                     ListarPets();
                 } else if (opcaoEscolhida == 4) {
@@ -52,55 +57,6 @@ public class AdopetConsoleApplication {
         }
     }
 
-    private static void ListarAbrigos() throws IOException, InterruptedException {
-
-        HttpClient client = HttpClient.newHttpClient();
-        // URL da API
-        String uri = "http://localhost:8080/abrigos";
-        HttpResponse<String> response = dispararGet(client, uri);
-        String responseBody = response.body();
-        JsonArray jsonArray = JsonParser.parseString(responseBody).getAsJsonArray();
-        System.out.println("Abrigos cadastrados:");
-
-        for (JsonElement element : jsonArray) {
-            JsonObject jsonObject = element.getAsJsonObject();
-            long id = jsonObject.get("id").getAsLong();
-            String nome = jsonObject.get("nome").getAsString();
-            System.out.println(id +" - " +nome);
-        }
-
-    }
-
-    private static void CadastrarAbrigo() throws IOException, InterruptedException {
-
-        System.out.println("Digite o nome do abrigo:");
-        String nome = new Scanner(System.in).nextLine();
-        System.out.println("Digite o telefone do abrigo:");
-        String telefone = new Scanner(System.in).nextLine();
-        System.out.println("Digite o email do abrigo:");
-        String email = new Scanner(System.in).nextLine();
-
-        JsonObject json = new JsonObject();
-        json.addProperty("nome", nome);
-        json.addProperty("telefone", telefone);
-        json.addProperty("email", email);
-
-        HttpClient client = HttpClient.newHttpClient();
-        String uri = "http://localhost:8080/abrigos";
-
-        HttpResponse<String> response = dispararPost(client, uri, json);
-
-        int statusCode = response.statusCode();
-        String responseBody = response.body();
-        if (statusCode == 200) {
-            System.out.println("Abrigo cadastrado com sucesso!");
-            System.out.println(responseBody);
-        } else if (statusCode == 400 || statusCode == 500) {
-            System.out.println("Erro ao cadastrar o abrigo:");
-            System.out.println(responseBody);
-        }
-
-    }
 
     private static void ListarPets() throws IOException, InterruptedException {
 
@@ -184,7 +140,7 @@ public class AdopetConsoleApplication {
     }
 
 
-    private static HttpResponse<String> dispararGet(HttpClient client, String uri) throws  IOException, InterruptedException{
+    public static HttpResponse<String> dispararGet(HttpClient client, String uri) throws  IOException, InterruptedException{
 
         //Conexão com a API executando o mét.odo GET restornando um HTTPRESPONSE<STRING>
         HttpRequest request = HttpRequest.newBuilder()
@@ -195,7 +151,7 @@ public class AdopetConsoleApplication {
 
     }
 
-    private static HttpResponse<String> dispararPost(HttpClient client, String uri, JsonObject json) throws IOException, InterruptedException{
+    public static HttpResponse<String> dispararPost(HttpClient client, String uri, JsonObject json) throws IOException, InterruptedException{
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
                 .header("Content-Type", "application/json")
